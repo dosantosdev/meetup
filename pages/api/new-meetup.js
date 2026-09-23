36a219d0072168368da5d69018e5ca61ffba4f6b
dosantosdev <dosantosdev@gmail.com>
2026-09-23 16:38:26 -0300

    feat: integrate MongoDB for meetups data management
    - Added MongoDB as a dependency in package.json.
    - Updated HomePage to fetch meetups from MongoDB in getStaticProps.
    - Removed dummy meetup data and mapped MongoDB results to meetups prop.
    - Implemented new API route for adding meetups to the database.
    - Enhanced NewMeetupPage to handle form submission and redirect after adding a meetup.

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,24 @@
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://deylamyllus_db_user:GPZPKlTy4jqHONRI@cluster0.tkxr1sx.mongodb.net/?appName=Cluster0",
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,31 @@
-import MeetupList from "@/components/meetups/MeetupList";
+import { MongoClient } from "mongodb";
 
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "A First Meetup",
-    image:
-      "https://www.melhoresdestinos.com.br/wp-content/uploads/2023/01/pelourinho-salvador-bahia-capa-2023.jpg",
-    address: "Pelourinho - Salvador - BA",
-    description: "This is a first meetup",
-  },
-  {
-    id: "m2",
-    title: "A Second Meetup",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTpPoREkBrY2f4PmFBF5fu5IrT5kh8yNuCdMtMPqTnbQ&s=10",
-    address: "Igreja em algum lugar",
-    description: "This is a second meetup",
-  },
-];
+import MeetupList from "@/components/meetups/MeetupList";
 
 function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
 
 export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://deylamyllus_db_user:GPZPKlTy4jqHONRI@cluster0.tkxr1sx.mongodb.net/?appName=Cluster0",
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
     revalidate: 1,
   };
