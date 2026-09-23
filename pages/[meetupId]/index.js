079d0821521187e12b4e3350ecc5f4ea0989f1b7
dosantosdev <dosantosdev@gmail.com>
2026-09-23 17:45:30 -0300

    feat: enhance meetup pages with dynamic data fetching from MongoDB; update HomePage and NewMeetupPage components for improved structure and SEO

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,48 +1,72 @@
-import MeetupDetail from "@/components/meetups/MeetupDetail";
+import { MongoClient, ObjectId } from "mongodb";
 
-function MeetupDetails() {
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+import { Fragment } from "react";
+import Head from "next/head";
+
+function MeetupDetails(props) {
   return (
-    <MeetupDetail
-      image="https://www.melhoresdestinos.com.br/wp-content/uploads/2023/01/pelourinho-salvador-bahia-capa-2023.jpg"
-      title="A First Meetup"
-      address="Pelourinho - Salvador - BA"
-      description="This is a first meetup"
-    />
+    <Fragment>
+      <Head>
+        <title>{props.meetupData.title}</title>
+        <meta name="description" content={props.meetupData.description} />
+      </Head>
+      <MeetupDetail
+        image={props.meetupData.image}
+        title={props.meetupData.title}
+        address={props.meetupData.address}
+        description={props.meetupData.description}
+      />
+    </Fragment>
   );
 }
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://deylamyllus_db_user:GPZPKlTy4jqHONRI@cluster0.tkxr1sx.mongodb.net/?appName=Cluster0",
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
   return {
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
-      },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-    ],
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
   };
 }
 
 export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
 
-  console.log(meetupId);
+  const client = await MongoClient.connect(
+    "mongodb+srv://deylamyllus_db_user:GPZPKlTy4jqHONRI@cluster0.tkxr1sx.mongodb.net/?appName=Cluster0",
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: new ObjectId(meetupId),
+  });
+
+  console.log(selectedMeetup);
+
+  client.close();
 
   return {
     props: {
       meetupData: {
-        image:
-          "https://www.melhoresdestinos.com.br/wp-content/uploads/2023/01/pelourinho-salvador-bahia-capa-2023.jpg",
-        id: meetupId,
-        title: "First Meetup",
-        address: "Pelourinho - Salvador - BA",
-        description: "This is a first meetup",
+        id: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        image: selectedMeetup.image,
+        description: selectedMeetup.description,
       },
     },
   };
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,5 +1,7 @@
 import NewMeetupForm from "@/components/meetups/NewMeetupForm";
 import { useRouter } from "next/router";
+import Head from "next/head";
+import { Fragment } from "react";
 
 function NewMeetupPage() {
   const router = useRouter();
@@ -19,7 +21,18 @@ function NewMeetupPage() {
 
     router.push("/");
   }
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a New Meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetups and create amazing networking opportunities."
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </Fragment>
+  );
 }
 
 export default NewMeetupPage;
